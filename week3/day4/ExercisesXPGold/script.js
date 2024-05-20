4f223accd425cc20f2ddbf0de968b9ee0bbd6543
mawasserman <wasserman.mv@gmail.com>
2024-05-20 22:11:05 +0300

    week3 day4 exerciseXP gold done

--- week3/day4/ExercisesXPGold/script.js
+++ week3/day4/ExercisesXPGold/script.js
@@ -51,9 +51,22 @@ else{
 // Exercise 3 : Verbing
 // Instructions
 // Prompt the user for a string. It must be a verb.
+let userVerb = prompt("Please give me a verb");
+
 // If the length of the string is at least 3 and the string doesn’t end with “ing”, add “ing” to the end of the string.
-// If the length of the string is at least 3 and the string ends with “ing” add “ly” to it’s end.
-// If the length of the string is less than 3, leave it unchanged.
+if(userVerb.length >=3){
+    if(userVerb.indexOf("ing") == (userVerb.length -3)){
+        console.log(userVerb + 'ly');
+    }
+    else {
+        console.log(userVerb + 'ing')
+    }
+        // If the length of the string is at least 3 and the string ends with “ing” add “ly” to it’s end.
+}// If the length of the string is less than 3, leave it unchanged.
+
+else{
+    console.log(userVerb)
+}
 // Example:
 
 //   The string is : "read" , your program should console.log : "reading"
