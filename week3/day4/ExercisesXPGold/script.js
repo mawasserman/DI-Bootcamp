daeef92061726f71082e90d6b4b443ad2f04fe07
mawasserman <wasserman.mv@gmail.com>
2024-05-20 21:52:59 +0300

    2 done

--- week3/day4/ExercisesXPGold/script.js
+++ week3/day4/ExercisesXPGold/script.js
@@ -29,13 +29,24 @@ switch (lowUserLanguage) {
 // Exercise 2 : The Grade Assigner
 // Instructions
 // Ask the user for their grade.
+let userGrade = prompt("What is your grade?");
 
 // If the grade is bigger than 90, console.log “A”
-
+if(userGrade > 90){
+    console.log("A");
+}  
 // If the grade is between 80 and 90 (included), console.log “B”
+else if(userGrade > 80 && userGrade <= 90){
+    console.log("B");
+}
 // If the grade is between 70(included) and 80 (included), console.log “C”
+else if(userGrade >= 70 && userGrade <= 80){
+    console.log("C");
+}
 // If the grade is lower than 70, console.log “D”
-
+else{
+    console.log("D");
+}
 
 // Exercise 3 : Verbing
 // Instructions
