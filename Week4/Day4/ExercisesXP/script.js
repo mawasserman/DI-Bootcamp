15b1f7a4e303278bb6bcc3385b733663d464860a
mawasserman <wasserman.mv@gmail.com>
2024-05-27 21:33:56 +0300

    week4 day4 exercisesXP 3 done (after yoga)

--- Week4/Day4/ExercisesXP/script.js
+++ Week4/Day4/ExercisesXP/script.js
@@ -60,14 +60,27 @@
 // 🌟 Exercise 3 : Change The Navbar
 
 // Using Javascript, in the <div>, change the value of the id attribute from navBar to socialNetworkNavigation, using the setAttribute method.
+const divExercice3 = document.querySelector('div');
+divExercice3.setAttribute('id', 'socialNetworkNavigation');
+console.log(divExercice3); //first time forgot to put id inside the quotes and it didn't work
 
 // We are going to add a new <li> to the <ul>.
 // First, create a new <li> tag (use the createElement method).
+const newLi = document.createElement('li');
+
 // Create a new text node with “Logout” as its specified text.
+const textNode = document.createTextNode('Logout');
+
 // Append the text node to the newly created list node (<li>).
+newLi.appendChild(textNode);
+
 // Finally, append this updated list node to the unordered list (<ul>), using the appendChild method.
+const ulExercise3 = divExercice3.firstElementChild;
+ulExercise3.appendChild(newLi);
 
 // Use the firstElementChild and the lastElementChild properties to retrieve the first and last <li> elements from their parent element (<ul>). Display the text of each link. (Hint: use the textContent property).
+console.log(ulExercise3.firstElementChild.textContent);
+console.log(ulExercise3.lastElementChild.textContent);
 
 
 // 🌟 Exercise 4 : My Book List
