0250ab14a98466b5ebe1fde0cdc59c4bd50a4279
mawasserman <wasserman.mv@gmail.com>
2024-06-04 20:46:18 +0300

    apanhando do grid

---
+++ Week5/Day5/script.js
@@ -0,0 +1,6 @@
+// JS:
+
+// How does a user choose a color?
+// How does drawing work? How can you tell when the user is clicking and dragging?
+// Hint, look at the JS events mousedown, mouseup, and mouseover.
+
