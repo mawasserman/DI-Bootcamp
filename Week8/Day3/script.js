c3c83f109cd9dd4e6347c982018e54746c277a7f
mawasserman <wasserman.mv@gmail.com>
2024-06-25 20:56:50 +0300

    aula 25062024

---
+++ Week8/Day3/script.js
@@ -0,0 +1,27 @@
+/*
+AULA DIA 25/06/2024
+
+se escrever {...nome do treco} a gente vai clonar... se clonar não vai ser referencia, vai ser algo individual... então se mudar um não muda o outro.. diferente de quando tem
+groceries2 = groceries1 que rola hosting, ai é referencia, se mudar um muda o outro 
+
+DEEP CLONE
+
+é uma tecnica de json.
+json é um formato de dados que é uma string, e é um formato de dados que é muito parecido com o objeto literal do javascript, mas é uma string, e é um formato de dados que é muito usado para transferir dados entre servidores e clientes, e entre servidores e servidores, e entre clientes e clientes
+
+JSON.stringify(objeto) -> transforma um objeto em uma string
+JSON.parse(string) -> transforma uma string em um objeto
+----> isso é uma forma de fazer um deep clone
+
+por que eu faria um deep clone? para não ter referencia, para ter um objeto individual
+
+map -> é um metodo de array que cria um novo array com base em um array existente, e ele cria um novo array com base em uma função que você passa para ele
+
+não to conseguindo concentrar na aula do Ziv... eu não gosto da maneira dele...
+
+.filter -> é um metodo de array que cria um novo array com base em um array existente, e ele cria um novo array com base em uma função que você passa para ele, e essa função tem que retornar true ou false, e ele vai criar um novo array com base nos elementos que retornaram true...
+ele retorna o item da array
+
+
+
+*/
