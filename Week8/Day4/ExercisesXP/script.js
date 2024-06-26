0cb16cc8c1d418cea63cf3f2828b5e5f82ebe879
mawasserman <wasserman.mv@gmail.com>
2024-06-26 20:14:20 +0300

    week8 day4 exercisesXP done

--- Week8/Day4/ExercisesXP/script.js
+++ Week8/Day4/ExercisesXP/script.js
@@ -26,54 +26,67 @@
 
 
 
-// 🌟 Exercise 2 : Employees
-// Instructions
-// Using this array:
+// // 🌟 Exercise 2 : Employees
+// // Instructions
+// // Using this array:
 
-const users = [{ firstName: 'Bradley', lastName: 'Bouley', role: 'Full Stack Resident' },
-             { firstName: 'Chloe', lastName: 'Alnaji', role: 'Full Stack Resident' },
-             { firstName: 'Jonathan', lastName: 'Baughn', role: 'Enterprise Instructor' },
-             { firstName: 'Michael', lastName: 'Herman', role: 'Lead Instructor' },
-             { firstName: 'Robert', lastName: 'Hajek', role: 'Full Stack Resident' },
-             { firstName: 'Wes', lastName: 'Reid', role: 'Instructor'},
-             { firstName: 'Zach', lastName: 'Klabunde', role: 'Instructor'}];
+// const users = [{ firstName: 'Bradley', lastName: 'Bouley', role: 'Full Stack Resident' },
+//              { firstName: 'Chloe', lastName: 'Alnaji', role: 'Full Stack Resident' },
+//              { firstName: 'Jonathan', lastName: 'Baughn', role: 'Enterprise Instructor' },
+//              { firstName: 'Michael', lastName: 'Herman', role: 'Lead Instructor' },
+//              { firstName: 'Robert', lastName: 'Hajek', role: 'Full Stack Resident' },
+//              { firstName: 'Wes', lastName: 'Reid', role: 'Instructor'},
+//              { firstName: 'Zach', lastName: 'Klabunde', role: 'Instructor'}];
 
-// Using the map() method, push into a new array the firstname of the user and a welcome message. You should get an array that looks like this :
-// const welcomeStudents = ["Hello Bradley", "Hello Chloe", "Hello Jonathan", "Hello Michael", "Hello Robert", "Hello Wes", "Hello Zach"]
-let welcomeStudents = users.map((user, index) => `Hello ${user.firstName}`);
-console.log(welcomeStudents);
+// // Using the map() method, push into a new array the firstname of the user and a welcome message. You should get an array that looks like this :
+// // const welcomeStudents = ["Hello Bradley", "Hello Chloe", "Hello Jonathan", "Hello Michael", "Hello Robert", "Hello Wes", "Hello Zach"]
+// let welcomeStudents = users.map((user, index) => `Hello ${user.firstName}`);
+// console.log(welcomeStudents);
 
-// 2. Using the filter() method, create a new array, containing only the Full Stack Residents.
-let test = users.filter(user => user.role === 'Full Stack Resident'); 
-let fullStackResidents = test.map((test) => `${test.firstName} ${test.lastName}`);
-//before i did the filter on the fullStackResidents, but I was getting an object, so I decided to map it to get the full name in an array
-console.log(fullStackResidents);
+// // 2. Using the filter() method, create a new array, containing only the Full Stack Residents.
+// let test = users.filter(user => user.role === 'Full Stack Resident'); 
+// let fullStackResidents = test.map((test) => `${test.firstName} ${test.lastName}`);
+// //before i did the filter on the fullStackResidents, but I was getting an object, so I decided to map it to get the full name in an array
+// console.log(fullStackResidents);
 
-// 3. Bonus : Chain the filter method with a map method, to return an array containing only the lastName of the Full Stack Residents.
-//So.... It is basically the same as the previous question, but I will map it to get only the last name and add here my first try of the filter
-let fullStackResidentsLastName = users.filter(user => user.role === 'Full Stack Resident').map((user) => user.lastName);
-console.log(fullStackResidentsLastName);
+// // 3. Bonus : Chain the filter method with a map method, to return an array containing only the lastName of the Full Stack Residents.
+// //So.... It is basically the same as the previous question, but I will map it to get only the last name and add here my first try of the filter
+// let fullStackResidentsLastName = users.filter(user => user.role === 'Full Stack Resident').map((user) => user.lastName);
+// console.log(fullStackResidentsLastName);
 
-//previous of the ex. 2: let fullStackResidents = users.filter(user => user.role === 'Full Stack Resident'); 
+// //previous of the ex. 2: let fullStackResidents = users.filter(user => user.role === 'Full Stack Resident'); 
 
 
 
 // 🌟 Exercise 3 : Star Wars
 // Instructions
-// Using this array const epic = ['a', 'long', 'time', 'ago', 'in a', 'galaxy', 'far far', 'away'];
+// Using this array 
+const epic = ['a', 'long', 'time', 'ago', 'in a', 'galaxy', 'far far', 'away'];
 
 // Use the reduce() method to combine all of these into a single string.
 
+const singleString = epic.reduce((epic, word) => epic + ' ' + word, '');
+console.log(singleString);
 
 // 🌟 Exercise 4 : Employees #2
 // Instructions
 // Using this object:
 
-// const students = [{name: "Ray", course: "Computer Science", isPassed: true}, 
-//                {name: "Liam", course: "Computer Science", isPassed: false}, 
-//                {name: "Jenner", course: "Information Technology", isPassed: true}, 
-//                {name: "Marco", course: "Robotics", isPassed: true}, 
-//                {name: "Kimberly", course: "Artificial Intelligence", isPassed: false}, 
-//                {name: "Jamie", course: "Big Data", isPassed: false}];
+const students = [{name: "Ray", course: "Computer Science", isPassed: true}, 
+               {name: "Liam", course: "Computer Science", isPassed: false}, 
+               {name: "Jenner", course: "Information Technology", isPassed: true}, 
+               {name: "Marco", course: "Robotics", isPassed: true}, 
+               {name: "Kimberly", course: "Artificial Intelligence", isPassed: false}, 
+               {name: "Jamie", course: "Big Data", isPassed: false}];
+
 // Using the filter() method, create a new array, containing the students that passed the course.
+let passedStudents = students.filter(student => student.isPassed === true);
+console.log(passedStudents);
+
 // Bonus : Chain the filter method with a forEach method, to congratulate the students with their name and course name (ie. “Good job Jenner, you passed the course in Information Technology”, “Good Job Marco you passed the course in Robotics” ect…)
+function congratulateStudents(passedStudents) {
+    passedStudents.forEach((student) => {
+        console.log(`Good job ${student.name}, you passed the course in ${student.course}`);
+    });
+}
+congratulateStudents(passedStudents);
