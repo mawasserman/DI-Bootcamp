92c78f22cf6d670e359fb32f37c8f3cae5b518ca
mawasserman <wasserman.mv@gmail.com>
2024-08-27 21:53:21 +0300

    w14d2 xp 1 2 done

---
+++ Redux_aulaExtra/classnotes.js
@@ -0,0 +1,23 @@
+/**
+ * Redux é uma biblioteca que ajuda a gerenciar o estado da aplicação.
+ * se usa redux com react, mas pode ser usado com qualquer biblioteca ou framework.
+ * 
+ * Redux é um container de estado previsível para aplicativos JavaScript.
+ * 
+ * Redux é um padrão de arquitetura que ajuda a escrever aplicativos que se comportam de maneira consistente em diferentes ambientes (cliente, servidor e nativo) e são fáceis de testar.
+ * 
+ * tbm é importante aprender a usar Angular, Vue, Svelte, etc.
+ * 
+ * Redux e TypeScript são o minimo hoje pra conseguir um emprego... mas eu vi tbm Angular...
+ * 
+ * criei um novo projeto com o vite, com react e javascript normal, depois coloquei o redux. 
+ * >>> npm i redux @reduxjs/toolkit react-redux
+ * 
+ * e só depois vai clicar como run dev
+ * 
+ * na pasta de src, criar pasta rudux, e dentro dela criar 3 arquivos js... actions.js, reducers.js, store.js
+ * 
+ * 
+ * 
+ * 
+ */
