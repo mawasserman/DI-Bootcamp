d5e80e30c95512c4472079bc27eb63ccdb6c39d7
mawasserman <wasserman.mv@gmail.com>
2024-09-22 20:58:27 +0300

    class w20

---
+++ Week20/Day1/notesDeployment.js
@@ -0,0 +1,6 @@
+/**
+ * Deployment
+ * 
+ * a gente vai fazer download do arquivo zip fora da pasta do di-bootcamp
+ * C:\Users\User\Documents\Marcella\Estudo\Programacao\Developers Institute\deploy_server-main
+ */
