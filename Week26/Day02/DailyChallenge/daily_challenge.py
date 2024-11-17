62e67cdeb7fb7c6f4175001b7253616a117e55a7
mawasserman <wasserman.mv@gmail.com>
2024-11-17 18:51:32 +0200

    first commmit mini project w26d5

---
+++ Week26/Day02/DailyChallenge/daily_challenge.py
@@ -0,0 +1,43 @@
+# Instructions
+# Given a “Matrix” string:
+
+#     7ii
+#     Tsx
+#     h%?
+#     i #
+#     sM 
+#     $a 
+#     #t%
+#     ^r!
+
+
+# The matrix is a grid of strings (alphanumeric characters and spaces) with a hidden message in it.
+# A grid means that you could potentially break it into rows and columns, like here:
+
+# 7	i	i
+# T	s	x
+# h	%	?
+# i		#
+# s	M	
+# $	a	
+# #	t	%
+# ^	r	!
+
+
+# Matrix: A matrix is a two-dimensional array. It is a grid of numbers arranged in rows and columns.
+# To reproduce the grid, the matrix should be a 2D list, not a string
+
+
+
+# To decrypt the matrix, Neo reads each column from top to bottom, starting from the leftmost column, selecting only the alpha characters and connecting them. Then he replaces every group of symbols between two alpha characters by a space.
+
+# Using his technique, try to decode this matrix.
+
+# Hints:
+# Use
+# ● lists for storing data
+# ● Loops for going through the data
+# ● if/else statements to check the data
+# ● String for the output of the secret message
+
+# Hint (if needed) : Look at the remote learning “Matrix” video.
---
+++ Week26/Day05/MiniProject/mini_project.py
@@ -0,0 +1,36 @@
+# Create a TicTacToe game in python, where two users can play together.
+
+# Instructions
+# The game is played on a grid that’s 3 squares by 3 squares.
+# Players take turns putting their marks (O or X) in empty squares.
+# The first player to get 3 of their marks in a row (up, down, across, or diagonally) is the winner.
+# When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie.
+
+
+# Hint
+# To do this project, you basically need to create four functions:
+
+# display_board() – To display the Tic Tac Toe board (GUI).
+display_board()
+
+
+# player_input(player) – To get the position from the player.
+player_input(player)
+
+
+# check_win() – To check whether there is a winner or not.
+check_win()
+
+
+# play() – The main function, which calls all the functions created above.
+play()
+
+
+# Note: The 4 functions above are just an example. You can implement many more helper functions or choose a completely different appoach if you want.
+
+
+
+# Tips : Consider the following:
+# What functionality will need to accur every turn to make this program work?
+# After contemplating the question above, think about splitting your code into smaller pieces (functions).
+# Remember to follow the single responsibility principle! each function should do one thing and do it well!
