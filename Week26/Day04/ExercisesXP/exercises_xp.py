2e4114b2b7f193612e2e6e95c883b53291eb798a
mawasserman <wasserman.mv@gmail.com>
2024-11-27 20:21:08 +0200

    w26d4 exXP 1 done

---
+++ Week26/Day04/ExercisesXP/exercises_xp.py
@@ -0,0 +1,141 @@
+# 🌟 Exercise 1 : Temperature Advice
+# Instructions
+# Create a function called get_random_temp().
+# This function should return an integer between -10 and 40 degrees (Celsius), selected at random.
+# Test your function to make sure it generates expected results.
+# import random
+
+# def get_random_temp():  #first time doing this function...
+#     return random.randint(-10, 40)
+# print(get_random_temp())
+
+import random
+
+def get_random_temp(season):
+    if season == "winter":
+        return random.uniform(-10, 16)
+    elif season == "spring":
+        return random.uniform(0, 25)
+    elif season == "summer":
+        return random.uniform(20, 40)
+    elif season == "autumn" or season == "fall":
+        return random.uniform(10, 30)
+    else:
+        print("That's not a valid season. Please enter winter, spring, summer, or autumn.")
+        return None
+
+# Create a function called main().
+# Inside this function, call get_random_temp() to get a temperature, and store its value in a variable.
+# Inform the user of the temperature in a friendly message, eg. “The temperature right now is 32 degrees Celsius.”
+# Let’s add more functionality to the main() function. Write some friendly advice relating to the temperature:
+# below zero (eg. “Brrr, that’s freezing! Wear some extra layers today”)
+# between zero and 16 (eg. “Quite chilly! Don’t forget your coat”)
+# between 16 and 23
+# between 24 and 32
+# between 32 and 40
+
+def main():
+    season = input("What season is it? ").lower().strip()
+    temp = get_random_temp(season)
+    if temp is None:  
+        print("Unable to determine temperature due to invalid input.")
+        return
+    temp = round(temp, 2)
+    print(f"The temperature right now is {temp} degrees Celsius.")
+    if temp < 0:
+        print("Brrr, that’s freezing! Wear some extra layers today")
+    elif temp >= 0 and temp < 16:
+        print("Quite chilly! Don’t forget your coat")
+    elif temp >= 16 and temp < 24:
+        print("Still pretty cold for Brazilians... Sleep with thousands of blankets!")
+    elif temp >= 24 and temp < 32:
+        print("Regular winter in Rio de Janeiro! Get a jacket!")
+    elif temp >= 32 and temp <= 40:
+        print("If you are in Rio de Janeiro, thank HaShem that is not summer yet!")
+    else:
+        return None
+
+main()
+
+# Change the get_random_temp() function:
+# Add a parameter to the function, named ‘season’.
+# Inside the function, instead of simply generating a random number between -10 and 40, set lower and upper limits based on the season, eg. if season is ‘winter’, temperatures should only fall between -10 and 16.
+# Now that we’ve changed get_random_temp(), let’s change the main() function:
+# Before calling get_random_temp(), we will need to decide on a season, so that we can call the function correctly. Ask the user to type in a season - ‘summer’, ‘autumn’ (you can use ‘fall’ if you prefer), ‘winter’, or ‘spring’.
+# Use the season as an argument when calling get_random_temp().
+
+# Bonus: Give the temperature as a floating-point number instead of an integer.
+# Bonus: Instead of asking for the season, ask the user for the number of the month (1 = January, 12 = December). Determine the season according to the month.
+
+
+# 🌟 Exercise 2 : Star Wars Quiz
+# Instructions
+# This project allows users to take a quiz to test their Star Wars knowledge.
+# The number of correct/incorrect answers are tracked and the user receives different messages depending on how well they did on the quiz.
+
+# Here is an array of dictionaries, containing those questions and answers
+
+# data = [
+#     {
+#         "question": "What is Baby Yoda's real name?",
+#         "answer": "Grogu"
+#     },
+#     {
+#         "question": "Where did Obi-Wan take Luke after his birth?",
+#         "answer": "Tatooine"
+#     },
+#     {
+#         "question": "What year did the first Star Wars movie come out?",
+#         "answer": "1977"
+#     },
+#     {
+#         "question": "Who built C-3PO?",
+#         "answer": "Anakin Skywalker"
+#     },
+#     {
+#         "question": "Anakin Skywalker grew up to be who?",
+#         "answer": "Darth Vader"
+#     },
+#     {
+#         "question": "What species is Chewbacca?",
+#         "answer": "Wookiee"
+#     }
+# ]
+
+
+# Create a function that asks the questions to the user, and check his answers. Track the number of correct, incorrect answers. Create a list of wrong_answers
+# Create a function that informs the user of his number of correct/incorrect answers.
+# Bonus : display to the user the questions he answered wrong, his answer, and the correct answer.
+# If he had more then 3 wrong answers, ask him to play again.
+
+
+# Exercise 3 : When will I retire ?
+# Instructions
+# The point of the exercise is to check if a person can retire depending on their age and their gender.
+# Note : Let’s say retirement age is 67 for men, and 62 for women (born after April, 1947).
+
+# Create a function get_age(year, month, day)
+# Hard-code the current year and month in your code (there are better ways of doing this, but for now it will be enough.)
+# After calculating the age of a person, the function should return the age (the age is an integer).
+# Create a function can_retire(gender, date_of_birth).
+# It should call the get_age function (with what arguments?) in order to receive an age.
+# Now it has all the information it needs in order to determine if the person with the given gender and date of birth is able to retire or not.
+# Calculate. You may need to do a little more hard-coding here.
+# Return True if the person can retire, and False if he/she can’t.
+# Some Hints
+
+# Ask for the user’s gender as “m” or “f”.
+# Ask for the user’s date of birth in the form of “yyyy/mm/dd”, eg. “1993/09/21”.
+# Call can_retire to get a definite value for whether the person can or can’t retire.
+# Display a message informing the user whether they can retire or not.
+# As always, test your code to ensure it works.
+
+
+# Exercise 4:
+# Instructions
+# Write a function that accepts one parameter (an int: X) and returns the value of X+XX+XXX+XXXX.
+# Example:
+
+# If X=3, the output when calling our function should be 3702 (3 + 33 + 333 + 3333)
+
+# Hint: treating our number as a int or a str at different points in our code may be helpful
