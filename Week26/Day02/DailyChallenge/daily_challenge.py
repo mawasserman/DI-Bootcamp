dc812e5514de283ae07a3975027b9de878546ce0
mawasserman <wasserman.mv@gmail.com>
2024-11-27 15:49:43 +0200

    w26d2 daily challenge done

--- Week26/Day02/DailyChallenge/daily_challenge.py
+++ Week26/Day02/DailyChallenge/daily_challenge.py
@@ -13,21 +13,33 @@
 
 # The matrix is a grid of strings (alphanumeric characters and spaces) with a hidden message in it.
 # A grid means that you could potentially break it into rows and columns, like here:
-
-# 7	i	i
-# T	s	x
-# h	%	?
-# i		#
-# s	M	
-# $	a	
-# #	t	%
-# ^	r	!
-
-
 # Matrix: A matrix is a two-dimensional array. It is a grid of numbers arranged in rows and columns.
 # To reproduce the grid, the matrix should be a 2D list, not a string
 
+matrix = [
+    ["7", "i", "i"],
+    ["T", "s", "x"],
+    ["h", "%", "?"],
+    ["i", " ", "#"],
+    ["s", "M", " "],
+    ["$", "a", " "],
+    ["#", "t", "%"],
+    ["^", "r", "!"]
+]
 
+decrypted_matrix = ""
+
+for column in range(len(matrix[0])):
+    for row in range(len(matrix)):
+        char = matrix[row][column]
+        if char.isalpha():
+            decrypted_matrix += char
+        else:
+            if decrypted_matrix and decrypted_matrix[-1] != " ":
+                decrypted_matrix += " "
+
+decrypted_matrix = " ".join(decrypted_matrix.split())
+print(decrypted_matrix)
 
 # To decrypt the matrix, Neo reads each column from top to bottom, starting from the leftmost column, selecting only the alpha characters and connecting them. Then he replaces every group of symbols between two alpha characters by a space.
 
