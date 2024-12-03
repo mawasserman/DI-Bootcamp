468e6ba4e5255a4a216d6db8ca956cbce3c7fd00
mawasserman <wasserman.mv@gmail.com>
2024-12-03 22:40:12 +0200

    w28d2 daily challenge done

--- Week28/Day02/DailyChallenge/daily_challenge.py
+++ Week28/Day02/DailyChallenge/daily_challenge.py
@@ -13,30 +13,58 @@
 # Bonus (not mandatory) : Install the Turtle module, and draw the sorted circles
 
 class Circle:
-    def __init__(self, radius = 1):
-        self.radius = radius
-        self.diameter = 2 * radius
+    def __init__(self, radius=None, diameter=None):
+        if radius is not None:
+            self.radius = radius
+        elif diameter is not None:
+            self.radius = diameter / 2
+        else:
+            raise ValueError("You must provide either a radius or a diameter.")
 
     def area(self):
-        return round(3.14 * self.radius**2, 2)    
-    
+        return round(3.14 * (self.radius**2), 2)
+
     def __str__(self):
-        return f"Circle with radius: {self.radius} equals to diameter: {self.diameter}\nArea: {self.area()}"
-    
+        return f"Circle with radius: {self.radius} (diameter: {self.radius * 2})\nArea: {self.area()}"
+
     def __add__(self, other):
-        return Circle(self.radius + other.radius)
-    
-    def __gt__(self, other):
-        if self.radius > other.radius :
+        if isinstance(other, Circle):
+            return Circle(self.radius + other.radius)
+        raise TypeError("Can only add another Circle.")
+
+    def __lt__(self, other):
+        if isinstance(other, Circle):
+            return self.radius < other.radius
+        raise TypeError("Can only compare with another Circle.")
+
+    def compare(self, other):
+        if self.radius > other.radius:
             print(f"{self.radius} is bigger than {other.radius}")
         elif self.radius < other.radius:
             print(f"{self.radius} is smaller than {other.radius}")
         else:
             print(f"{self.radius} is equal to {other.radius}")
-    
-    def l
 
 
-    
+# Testing the Circle class
+circle1 = Circle(None, 6)
+circle2 = Circle(5, None)
+circle3 = Circle(2)
+
+print(circle1)
+print(circle2)
+
+circle4 = circle1 + circle2
+print(f"Circle 1 + Circle 2:\n{circle4}")
+
+circle1.compare(circle2)
+circle1.compare(circle3)
+circle2.compare(circle3)
+
+circles = [circle1, circle2, circle3]
+sorted_circles = sorted(circles)
+print("\nSorted Circles by radius:")
+for c in sorted_circles:
+    print(c)
 
 # # Joke of the day: Debugging is like being the detective in a crime drama where you are also the murderer.
