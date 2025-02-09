f7b19d514361cfe42e80b7ae876b4c755a44f69d
mawasserman <wasserman.mv@gmail.com>
2025-02-09 22:08:53 +0200

    more classes

--- Week9/Day2/ExercisesXP/script.js
+++ Week9/Day2/ExercisesXP/script.js
@@ -62,27 +62,27 @@ console.log(typeof member);
 // Instructions
 // Using the Dog class below:
 
-// class Dog {
-//   constructor(name) {
-//     this.name = name;
-//   }
-// };
+class Dog {
+  constructor(name) {
+    this.name = name;
+  }
+};
 // Analyze the options below. Which constructor will successfully extend the Dog class?
 //   // 1
-// class Labrador extends Dog {
-//   constructor(name, size) {
-//     this.size = size;
-//   }
-// };
+class Labrador extends Dog {
+  constructor(name, size) {
+    this.size = size;
+  }
+};
 
 
 //   // 2
-// class Labrador extends Dog {
-//   constructor(name, size) {
-//     super(name);
-//     this.size = size;
-//   }
-// };
+class Labrador extends Dog {
+  constructor(name, size) {
+    super(name);
+    this.size = size;
+  }
+};
 
 
 //   // 3
