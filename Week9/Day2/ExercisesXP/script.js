7f10c84d7878f5e3016f79de9d9d3867281ec2ed
mawasserman <wasserman.mv@gmail.com>
2025-04-22 10:30:02 +0300

    w9 d2 exXP DONE

--- Week9/Day2/ExercisesXP/script.js
+++ Week9/Day2/ExercisesXP/script.js
@@ -116,20 +116,43 @@ class Labrador extends Dog {
 
 // What is, for each object below, the value of the property number and why?
 
-// const object1 = { number: 5 }; 
-// const object2 = object1; 
-// const object3 = object2; 
-// const object4 = { number: 5};
+const object1 = { number: 5 }; 
+const object2 = object1; 
+const object3 = object2; 
+const object4 = { number: 5};
 
-// object1.number = 4;
-// console.log(object2.number)
-// console.log(object3.number)
-// console.log(object4.number)
+object1.number = 4;
+console.log(object2.number) // 4 object1, object2 and object3 are all pointing to the same object in memory
+console.log(object3.number) // 4
+console.log(object4.number) // 5 pointing to a different object in memory
 
 
 // Create a class Animal with the attributes name, type and color. The type is the animal type, for example: dog, cat, dolphin etc …
 
+class Animal {
+    constructor(name, type, color) {
+        this.name = name;
+        this.type = type;
+        this.color = color;
+    }
+}
+
 // Create a class Mammal that extends from the Animal class. Inside the class, add a method called sound(). This method takes a parameter: the sound the animal makes, and returns the details of the animal (name, type and color) as well as the sound it makes.
+
+class Mammal extends Animal {
+    constructor(name, type, color) {
+        super(name, type, color);
+    }
+
+    sound(sound) {
+        return `${sound} I'm a ${this.type}, named ${this.name} and I'm ${this.color}`;
+    }
+}
+
 
 // Create a farmerCow object that is an instance of the class Mammal. The object accepts a name, a type and a color and calls the sound method that “moos” her information.
 // For example: Moooo I'm a cow, named Lily and I'm brown and white
+
+const farmerCow = new Mammal('Lily', 'cow', 'brown and white');
+console.log(farmerCow.sound('Moooo')); // Moooo I'm a cow, named Lily and I'm brown and white
+
