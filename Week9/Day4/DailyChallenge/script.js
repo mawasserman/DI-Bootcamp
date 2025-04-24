871730fe68ec2e6634a8e444d79f6883229338d2
mawasserman <wasserman.mv@gmail.com>
2025-04-24 13:25:00 +0300

    w9 d4 dc

---
+++ Week9/Day4/DailyChallenge/script.js
@@ -0,0 +1,8 @@
+
+// Instructions
+// Create a form with two fields (name, last name) and a submit button.
+// When you click the Send button, retrieve the data from the inputs, and append it on the DOM as a JSON string.
+// The output should be:
+
+
+// output
