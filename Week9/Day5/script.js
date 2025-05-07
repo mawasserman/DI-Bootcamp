bb1f2550c2a547438f2ca270d58ddff0d4659046
mawasserman <wasserman.mv@gmail.com>
2025-05-07 15:12:14 +0300

    miniproject week9 day5

---
+++ Week9/Day5/script.js
@@ -0,0 +1,118 @@
+const container = document.getElementById('robots-container');
+
+const searchInput = document.getElementById('search');
+
+const robots = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz',
+      image: 'https://robohash.org/1?200x200'
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv',
+      image: 'https://robohash.org/2?200x200'
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net',
+      image: 'https://robohash.org/3?200x200'
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org',
+      image: 'https://robohash.org/4?200x200'
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca',
+      image: 'https://robohash.org/5?200x200'
+    },
+    {
+      id: 6,
+      name: 'Mrs. Dennis Schulist',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info',
+      image: 'https://robohash.org/6?200x200'
+    },
+    {
+      id: 7,
+      name: 'Kurtis Weissnat',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz',
+      image: 'https://robohash.org/7?200x200'
+    },
+    {
+      id: 8,
+      name: 'Nicholas Runolfsdottir V',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me',
+      image: 'https://robohash.org/8?200x200'
+    },
+    {
+      id: 9,
+      name: 'Glenna Reichert',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io',
+      image:'https://robohash.org/9?200x200'
+    },
+    {
+      id: 10,
+      name: 'Clementina DuBuque',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz',
+      image:'https://robohash.org/10?200x200'
+    }
+    ];
+
+
+    function displayRobots(robotsToShow){
+        container.innerHTML = ''
+
+
+        
+        robotsToShow.forEach(robot => {
+        const card = document.createElement('div');
+        card.className = 'card';
+      
+        const image = document.createElement('img');
+        image.src = robot.image;
+        image.alt = robot.name;
+      
+        const name = document.createElement('h2');
+        name.className = 'name';
+        name.textContent = robot.name;
+      
+        const email = document.createElement('p');
+        email.className = 'email';
+        email.textContent = robot.email;
+      
+        // all inside
+        card.appendChild(image);
+        card.appendChild(name);
+        card.appendChild(email);
+      
+        container.appendChild(card);
+    })};
+
+    displayRobots(robots);
+
+    searchInput.addEventListener('input', function () {
+        const searchText = searchInput.value.toLowerCase();
+      
+        const filteredRobots = robots.filter(robot =>
+          robot.name.toLowerCase().includes(searchText)
+        );
+      
+        displayRobots(filteredRobots);
+      });
