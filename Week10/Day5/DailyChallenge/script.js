1a838840321b78d4d4f0a3a916fa57c8675ec96f
mawasserman <wasserman.mv@gmail.com>
2025-06-10 10:12:07 +0300

    w10d5 dc done

--- Week10/Day5/DailyChallenge/script.js
+++ Week10/Day5/DailyChallenge/script.js
@@ -4,6 +4,7 @@ const toCurrency = document.querySelector("#toCurrency");
 const convertButton = document.querySelector("#convert");
 const result = document.querySelector("#result");
 const switchButton = document.querySelector("#switch");
+const converterForm = document.querySelector("#converterForm");
 
 fetch ("https://v6.exchangerate-api.com/v6/d341eee8b853c68a7c73eedc/codes")
     .then((response) => {
@@ -13,8 +14,51 @@ fetch ("https://v6.exchangerate-api.com/v6/d341eee8b853c68a7c73eedc/codes")
         return response.json();
     })
     .then((data) => {
-        console.log(data);
-    })
+        const currencies = data.supported_codes;
 
-    .catch();
-   
+        currencies.forEach(([code, name]) => {
+            const option1 = document.createElement("option");
+            option1.value = code;
+            option1.textContent = `${code} - ${name}`;
+            fromCurrency.appendChild(option1);
+
+            const option2 = document.createElement("option");
+            option2.value = code;
+            option2.textContent = `${code} - ${name}`;
+            toCurrency.appendChild(option2);
+    })})
+
+  .catch((error) => {
+        console.error("Error fetching currencies:", error);
+    });
+
+converterForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const amountValue = parseFloat(amount.value);
+    const fromCurrencyValue = fromCurrency.value;
+    const toCurrencyValue = toCurrency.value;
+
+    fetch(`https://v6.exchangerate-api.com/v6/d341eee8b853c68a7c73eedc/pair/${fromCurrencyValue}/${toCurrencyValue}/${amountValue}`)
+        .then((response) => {
+                    if (!response.ok) {
+                        throw new Error(`Error: ${response.status}`);
+                    }
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log(data);
+                    result.textContent = `${amountValue} ${fromCurrencyValue} = ${data.conversion_result} ${toCurrencyValue}`;
+                })
+                .catch((error) => {
+                    console.error("Error converting currency:", error);
+                });
+        });
+
+// switchButton.addEventListener("click", (e) => {
+//     e.preventDefault();
+
+//     const temp = fromCurrency.value;
+//     fromCurrency.value = toCurrency.value;
+//     toCurrency.value = temp;
+// });
